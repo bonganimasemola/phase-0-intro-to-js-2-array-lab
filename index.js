25eb23bcb36ea8fa9bbcd14ed443981808c5079c
bonganimasemola <bonganimasemola@icloud.com>
2023-09-25 08:42:35 +0300

    completed arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+
+
+/* function appendCat(cats) {
+    const newCat = "Broom";
+  cats.push(newCat); */ 
+
+function appendCat(name) {
+//let newCat = "Broom";  
+  const newCats = [...cats, name];
+  return newCats;
+}
+
+
+  
+
+function prependCat(name) {
+    const newCats = [ name, ...cats];
+    return newCats;
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, - 1);
+   
+  }
+
+ function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  } 
+
+
+ 
